a8e96359a2b782e68ecdfa09034ee8666d8e4e4b
Matt Fleming <mfleming2889@gmail.com>
2021-06-24 19:41:47 +0100

    netlify config updatyes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
-  target: "serverless",
-  distDir: "build"
 })
 
 module.exports = withBundleAnalyzer()
