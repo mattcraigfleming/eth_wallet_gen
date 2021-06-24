918866565b9fd5ae545d78fc4a3d4f2fba7fb029
Matt Fleming <mfleming2889@gmail.com>
2021-06-24 19:24:28 +0100

    inital commit, moved from private repo

---
+++ README.md
@@ -0,0 +1,43 @@
+# Getting Started
+
+Welcome to the light documentation of Ethereum Wallet Generator!
+
+## System Requirements
+
+- [Node.js](https://nodejs.org/en/) 10 or later
+- [ganache-cli](https://github.com/trufflesuite/ganache-cli)
+- MacOS, Windows (including WSL), and Linux are supported
+
+## Setup
+
+1. Run `yarn` or `npm i` to install dependencies.
+2. Run `yarn add -g ganache-cli` to global install local blockchain .
+3. Run `ganache-cli` to run local blockchain .
+4. Run `yarn dev` or `npm run dev` to see the example app at `http://localhost:3000`.
+
+These scripts refer to the different stages of developing an application:
+
+- `dev` - Runs `next` which starts Next.js in development mode
+- `build` - Runs `next` build which builds the application for production usage
+- `start:web` - Runs `next` start which starts a Next.js production server
+- `start:blockchain` - Runs `ganache-cli` start which starts a local Ethereum blockchain 
+
+Now you're ready to rumble! :traffic_light:
+
+## Documentation
+
+## Code of Conduct
+
+bla
+
+## Contributing
+
+Thank you for your interest in contributing; we are delighted you want to help out.
+
+## License
+
+This project is licensed under the MIT license
+
+## Authors
+
+- Anon
---
+++ pages/index.js
@@ -0,0 +1,101 @@
+import { useState } from 'react';
+import { Form, Select, Spin, Typography, Button, Card, message } from 'antd';
+import QRCode from 'react-qr-code';
+import Web3API from 'web3';
+import { PandaSvg } from '../assets/PandaSvg';
+
+const { Text, Paragraph } = Typography;
+const FormItem = Form.Item;
+const Option = Select.Option;
+
+const content = {
+  marginTop: '100px',
+};
+
+export default function Home() {
+  const [form] = Form.useForm();
+  const [account, setAccount] = useState({});
+  const [balance, setBalance] = useState('');
+  const [loading, setLoading] = useState(false);
+  const web3 = new Web3API(new Web3API.providers.HttpProvider('http://127.0.0.1:8545'));
+
+  const createAccount = async () => {
+    setLoading(true);
+    let acc = web3.eth.accounts.create(web3.utils.randomHex(32));
+    let newAcc = await acc;
+    let balance = await web3.eth.getBalance(newAcc.address);
+    console.log(balance);
+    console.log(newAcc);
+    console.log(web3.eth.providers);
+    setBalance(balance);
+    setAccount(newAcc);
+    setLoading(false);
+  };
+
+  const createWallet = async () => {
+    let wallet = web3.eth.accounts.wallet.create(1);
+    console.log(wallet);
+    message.success({
+      content: `Wallet created: ${wallet[0].address} with number of accounts: ${wallet.length}`,
+      className: 'custom-class',
+      style: {
+        marginTop: '20vh',
+      },
+    });
+    let encryptedWallet = wallet.encrypt(web3.utils.randomHex(32));
+    console.log(encryptedWallet)
+  };
+
+  const onReset = () => {
+    form.resetFields();
+    setAccount({});
+    setBalance('');
+  };
+
+  return (
+    <div style={content}>
+      <div className="text-center mb-5">
+        <PandaSvg />
+        <p className="mb-0 mt-3 text-disabled">Generate Ethereum Wallet!</p>
+      </div>
+      <div>
+        <Form form={form} layout="horizontal" onFinish={createAccount}>
+          <FormItem label="Wallet:" labelCol={{ span: 8 }} wrapperCol={{ span: 8 }}>
+            <Select size="large" defaultValue="eth" style={{ width: 192 }} name="walletSelect">
+              <Option value="eth">Ethereum</Option>
+              <Option value="btc" disabled>
+                Bitcoin
+              </Option>
+            </Select>
+          </FormItem>
+          <FormItem style={{ marginTop: 48 }} wrapperCol={{ span: 8, offset: 8 }}>
+            <Button size="large" type="primary" htmlType="submit">
+              Generate
+            </Button>
+            <Button size="large" style={{ marginLeft: 8 }} onClick={onReset}>
+              Cancel
+            </Button>
+            <Button size="large" style={{ marginLeft: 8 }} onClick={createWallet}>
+              Create Wallet
+            </Button>
+          </FormItem>
+        </Form>
+        {account.address ? (
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <Card size="small" title="Generated Account" extra={<a href="#">Reset</a>} style={{ width: 700 }}>
+              {loading ? <Spin /> : null}
+              <Paragraph copyable>
+                <span style={{ fontWeight: 'bold' }}>Public Key:</span> {account.address}
+              </Paragraph>
+              <Paragraph copyable>
+                <span style={{ fontWeight: 'bold' }}>Private Key: </span> {account.privateKey}
+              </Paragraph>
+              {balance ? <Text keyboard>Balance: {balance}</Text> : null}
+              <QRCode value={account.address} />
+            </Card>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
+}
